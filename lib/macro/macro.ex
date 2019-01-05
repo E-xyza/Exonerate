b90478c6ab38e1e370182401f2ed841a41c952f0
Isaac Yonemoto <isaac@rstor.io>
2019-01-05 14:42:21 -0800

    type specific testing works

--- lib/macro/macro.ex
+++ lib/macro/macro.ex
@@ -9,18 +9,29 @@ defmodule Exonerate.Macro do
     |> Jason.decode!
     |> matcher(method)
 
-    quote do
-      unquote(code)
+    code2 = quote do
+      unquote_splicing(code)
     end
+
+    IO.puts("")
+    code2
+    |> Macro.to_string
+    |> Code.format_string!
+    |> Enum.join
+    |> IO.puts
+
+    code2
   end
 
-  @spec matcher(any, any)::{:def, any, any} | {:__block__, any, any}
-  def matcher(map, method) when map == %{}, do: always_matches(method)
-  def matcher(true, method), do: always_matches(method)
+  @spec matcher(any, any)::[{:def, any, any} | {:__block__, any, any}]
+  def matcher(map, method) when map == %{}, do: [always_matches(method)]
+  def matcher(true, method), do: [always_matches(method)]
   def matcher(false, method), do: never_matches(method)
   def matcher(spec = %{"$schema" => schema}, method), do: match_schema(spec, schema, method)
   def matcher(spec = %{"$id" => id}, method), do: match_id(spec, id, method)
   def matcher(spec = %{"type" => "string"}, method), do: match_string(spec, method)
+  def matcher(spec = %{"type" => "number"}, method), do: match_number(spec, method)
+  def matcher(spec = %{"type" => list}, method) when is_list(list), do: match_list(spec, list, method)
 
   @spec always_matches(atom) :: {:def, any, any}
   defp always_matches(method) do
@@ -31,49 +42,80 @@ defmodule Exonerate.Macro do
     end
   end
 
-  @spec never_matches(atom) :: {:def, any, any}
+  @spec never_matches(atom) :: [{:def, any, any}]
   defp never_matches(method) do
-    quote do
+    [quote do
       def unquote(method)(val) do
-        {:mismatch, {__MODULE__, Atom.to_string(unquote(method))}, val}
+        {:mismatch, {__MODULE__, unquote(method)}, val}
       end
-    end
+    end]
   end
 
-  @spec match_schema(map, String.t, atom) :: {:__block__, any, any}
+  @spec match_schema(map, String.t, atom) :: [{:def, any, any}]
   def match_schema(map, schema, module) do
     rest = map
     |> Map.delete("$schema")
     |> matcher(module)
 
-    quote do
-      def schema, do: unquote(schema)
-      unquote(rest)
-    end
+
+    [quote do
+       def schema, do: unquote(schema)
+     end | rest]
   end
 
-  @spec match_id(map, String.t, atom) :: {:__block__, any, any}
+  @spec match_id(map, String.t, atom) :: [{:def, any, any}]
   def match_id(map, id, module) do
     rest = map
     |> Map.delete("$id")
     |> matcher(module)
 
-    quote do
+
+    [quote do
       def id, do: unquote(id)
-      unquote(rest)
-    end
+     end | rest]
   end
 
-  @spec match_string(map, atom) :: {:__block__, any, [{:def, any, any}]}
-  defp match_string(_spec, method) do
-    quote do
+  @spec match_string(map, atom, boolean) :: [{:def, any, any}]
+  defp match_string(_spec, method, terminal \\ true) do
+    str_match = quote do
       def unquote(method)(val) when is_binary(val) do
         :ok
       end
-      def unquote(method)(val) do
-        {:mismatch, {__MODULE__, Atom.to_string(unquote(method))}, val}
+    end
+
+    if terminal do
+      [str_match | never_matches(method)]
+    else
+      [str_match]
+    end
+  end
+
+  @spec match_number(map, atom, boolean) :: [{:def, any, any}]
+  defp match_number(_spec, method, terminal \\ true) do
+    num_match = quote do
+      def unquote(method)(val) when is_number(val) do
+        :ok
       end
     end
+
+    if terminal do
+      [num_match | never_matches(method)]
+    else
+      [num_match]
+    end
+  end
+
+  @spec match_list(map, list, atom) :: [{:def, any, any}]
+  defp match_list(_spec, [], method), do: never_matches(method)
+  defp match_list(spec, ["string" | tail], method) do
+    head_code = match_string(spec, method, false)
+    tail_code = match_list(spec, tail, method)
+    head_code ++ tail_code
+  end
+  defp match_list(spec, ["number" | tail], method) do
+    head_code = match_number(spec, method, false)
+    tail_code = match_list(spec, tail, method)
+    head_code ++ tail_code
   end
 
   defp maybe_desigil(s = {:sigil_s, _, _}) do
