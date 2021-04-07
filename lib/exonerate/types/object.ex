59427a78bc8e8f544cd294637278c8d4d68b8114
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2021-04-07 14:24:45 -0700

    better interface for checking object properties

--- lib/exonerate/types/object.ex
+++ lib/exonerate/types/object.ex
@@ -1,16 +1,43 @@
 defmodule Exonerate.Types.Object do
   @enforce_keys [:method]
-  @props ~w(min_properties max_properties property_names)a
+  @props ~w(
+    min_properties
+    max_properties
+    property_names
+    properties
+    additional_properties
+    )a
 
   defstruct @enforce_keys ++ @props
 
+  alias Exonerate.Types.String
+  alias Exonerate.Builder
+
   def build(method, params) do
+    properties = if props = params["properties"] do
+      props
+      |> Enum.map(fn {prop, spec} ->
+        {prop, Builder.to_struct(spec, :"#{method}-properties-#{prop}")}
+      end)
+      |> Map.new
+    end
+
+    property_names = if props = params["propertyNames"] do
+      String.build(:"#{method}-property_names_impl", props)
+    end
+
+    additional_properties = case params["additionalProperties"] do
+      default when default in [true, nil] -> nil
+      props -> Builder.to_struct(props, :"#{method}-additional_properties_impl")
+    end
+
     %__MODULE__{
       method: method,
       min_properties: params["minProperties"],
       max_properties: params["maxProperties"],
-      property_names: params["propertyNames"],
-      additional_properties: params["additionalProperties"]
+      properties:     properties,
+      property_names: property_names,
+      additional_properties: additional_properties
     }
   end
 
@@ -23,26 +50,17 @@ defmodule Exonerate.Types.Object do
     @guard_filters [:max_properties, :min_properties]
 
     def build(params = %{method: method}) do
-      filter_params = params
-      |> Map.take(Object.props() -- @guard_filters)
-      |> Enum.filter(&(elem(&1, 1)))
-      |> Enum.to_list
-
       guard_properties =
         size_branch(method, :<, params.min_properties) ++
         size_branch(method, :>, params.max_properties)
 
       quote do
         defp unquote(method)(value, path) when not is_map(value) do
-          {:mismatch, path, value}
+          {:mismatch, {path, value}}
         end
         unquote_splicing(guard_properties)
-        defp unquote(method)(object, path) do
-          unquote(next_call(filter_params, method))
-        end
-        defp unquote(method)(_, _), do: :ok
-
-        unquote_splicing(helpers(filter_params, method))
+        unquote(properties_filter(method, params.properties))
+        unquote_splicing(properties_helpers(method, params))
       end
     end
 
@@ -63,21 +81,47 @@ defmodule Exonerate.Types.Object do
       end
     end
 
-    defp helpers([{filter, value} | rest], method) do
-      [quote do
-        defp unquote(:"#{method}-#{filter}")(object, path) do
-          if unquote(filter_condition(filter, value)) do
-            unquote(next_call(rest, method))
-          else
-            {:mismatch, {path, object}}
-          end
-        end
-      end | helpers(rest, method)]
+    defp properties_filter(method, nil) do
+      quote do
+        defp unquote(method)(_object, _path), do: :ok
+      end
     end
-    defp helpers([], _), do: []
+    defp properties_filter(method, _) do
+      quote do
+        defp unquote(method)(object, path) do
+          Enum.each(object, fn {k, v} ->
+            unless (error = unquote(:"#{method}-properties")(k, v, path)) == :ok do
+              throw error
+            end
+          end)
+        catch
+          error = {:mismatch, _} -> error
+        end
+      end
+    end
 
-    defp filter_condition(:property_names, _value) do
-      true
+    defp properties_helpers(_method, %{properties: nil}), do: []
+    defp properties_helpers(method, params) do
+      {props, helpers} = params.properties
+      |> Enum.map(fn {k, v} ->
+        {
+          quote do
+            defp unquote(:"#{method}-properties")(unquote(k), property, path) do
+              unquote(v.method)(property, path <> "/properties/#{unquote(k)}")
+            end
+          end,
+          Exonerate.Buildable.build(v)
+        }
+      end)
+      |> Enum.unzip
+
+      default_prop = [quote do
+        defp unquote(:"#{method}-properties")(_, property, path) do
+          :ok
+        end
+      end]
+
+      props ++ default_prop ++ helpers
     end
   end
 end
