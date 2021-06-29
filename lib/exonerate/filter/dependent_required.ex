cf3b784d0c29c9b4fa5be4a7e6c8ff824d66c9d2
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2021-06-29 00:43:15 -0500

    knocks off dependent_schemas and dependent_required

--- lib/exonerate/filter/dependent_required.ex
+++ lib/exonerate/filter/dependent_required.ex
@@ -1,31 +1,44 @@
 defmodule Exonerate.Filter.DependentRequired do
   @behaviour Exonerate.Filter
 
+  alias Exonerate.Type
+  require Type
+
   @impl true
-  def append_filter(format, validation) do
-    format |> IO.inspect(label: "6")
-    #calls = validation.collection_calls
-    #|> Map.get(:array, [])
-    #|> List.insert_at(0, name(validation))
-#
-    #children = code(maximum, validation) ++ validation.children
-#
-    #validation
-    #|> put_in([:collection_calls, :array], calls)
-    #|> put_in([:children], children)
+  def append_filter(dependency, validation) when Type.is_schema(dependency) do
+    calls = validation.calls
+    |> Map.get(:object, [])
+    |> List.insert_at(0, name(validation))
+
+    children = List.insert_at(validation.children, 0, code(dependency, validation))
+
     validation
+    |> put_in([:calls, :object], calls)
+    |> put_in([:children], children)
   end
 
-  #defp name(validation) do
-  #  Exonerate.path(["maxItems" | validation.path])
-  #end
-#
-  #defp code(maximum, validation) do
-  #  [quote do
-  #     defp unquote(name(validation))({_, index}, acc, path) do
-  #       if index >= unquote(maximum), do: throw {:max, "maxItems"}
-  #       acc
-  #     end
-  #   end]
-  #end
+  defp name(validation) do
+    Exonerate.path(["dependentRequired" | validation.path])
+  end
+
+  defp code(dependencies, validation) do
+    calls = Enum.map(dependencies,
+      fn {key, otherkeys} when is_list(otherkeys) ->
+        quote do
+          defp unquote(name(validation))(object = %{unquote(key) => _}, path) do
+            unquote(otherkeys)
+            |> Enum.with_index
+            |> Enum.each(fn {otherkey, index} ->
+              unless is_map_key(object, otherkey) do
+                Exonerate.mismatch(object, path, guard: Path.join(unquote(key), to_string(index)))
+              end
+            end)
+          end
+        end
+      end)
+
+    calls ++ [quote do
+      defp unquote(name(validation))(_, _), do: :ok
+    end]
+  end
 end
--- lib/exonerate/filter/dependent_schemas.ex
+++ lib/exonerate/filter/dependent_schemas.ex
@@ -1,31 +1,45 @@
 defmodule Exonerate.Filter.DependentSchemas do
   @behaviour Exonerate.Filter
 
+  alias Exonerate.Type
+  require Type
+
   @impl true
-  def append_filter(sss, validation) do
-    sss |> IO.inspect(label: "6")
-    #calls = validation.collection_calls
-    #|> Map.get(:array, [])
-    #|> List.insert_at(0, name(validation))
-#
-    #children = code(maximum, validation) ++ validation.children
-#
-    #validation
-    #|> put_in([:collection_calls, :array], calls)
-    #|> put_in([:children], children)
+  def append_filter(dependency, validation) when Type.is_schema(dependency) do
+    calls = validation.calls
+    |> Map.get(:object, [])
+    |> List.insert_at(0, name(validation))
+
+    children = List.insert_at(validation.children, 0, code(dependency, validation))
+
     validation
+    |> put_in([:calls, :object], calls)
+    |> put_in([:children], children)
   end
 
-  #defp name(validation) do
-  #  Exonerate.path(["maxItems" | validation.path])
-  #end
-#
-  #defp code(maximum, validation) do
-  #  [quote do
-  #     defp unquote(name(validation))({_, index}, acc, path) do
-  #       if index >= unquote(maximum), do: throw {:max, "maxItems"}
-  #       acc
-  #     end
-  #   end]
-  #end
+  defp name(validation) do
+    Exonerate.path(["dependentSchemas" | validation.path])
+  end
+
+  # TODO: guard when object is the only type
+  # if object is the only type, avoid the guard.
+  defp code(dependencies, validation) do
+    {calls, funs} = dependencies
+    |> Enum.map(fn
+      {key, schema} when Type.is_schema(schema) ->
+        next_path = [key, "dependentSchemas" | validation.path]
+        {quote do
+          defp unquote(name(validation))(object = %{unquote(key) => _}, path) do
+            unquote(Exonerate.path(next_path))(object, path)
+          end
+        end,
+        Exonerate.Validation.from_schema(schema, next_path)
+        }
+      end)
+    |> Enum.unzip
+
+    calls ++ [quote do
+      defp unquote(name(validation))(_, _), do: :ok
+    end] ++ funs
+  end
 end
