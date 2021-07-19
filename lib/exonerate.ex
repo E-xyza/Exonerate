1a001c3df53319afe62e4b3f774149be0e0ad6ab
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2021-07-19 14:26:52 -0500

    forward to the correct file!

--- lib/exonerate.ex
+++ lib/exonerate.ex
@@ -146,7 +146,7 @@ defmodule Exonerate do
 
   @spec precache_file!(Path.t) :: binary
   @doc "lets you precache a file so you don't have to repeat loading it twice"
-  defdelegate precache_file!(path), to: Registry, as: :get_file
+  defdelegate precache_file!(path), to: Registry, as: :get_file!
 
   defp resolve_opts(opts, caller, defaults) do
     Enum.reduce(defaults, opts, fn {k, default}, opts ->
