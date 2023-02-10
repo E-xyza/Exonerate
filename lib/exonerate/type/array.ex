afded686ee47d314e7ea9ad7f838b4f6c16e4f93
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2023-02-10 11:37:26 -0600

    gets arrays compiling

--- lib/exonerate/type/array.ex
+++ lib/exonerate/type/array.ex
@@ -1,102 +1,16 @@
 defmodule Exonerate.Type.Array do
-  @moduledoc false
-  # boilerplate!!
-  @behaviour Exonerate.Type
-  @derive Exonerate.Compiler
-  @derive {Inspect, except: [:context]}
-
-  alias Exonerate.Filter
   alias Exonerate.Tools
-  alias Exonerate.Context
 
-  defstruct [
-    :context,
-    :additional_items,
-    filters: [],
-    pipeline: [],
-    needs_array_in_accumulator: false,
-    needs_accumulator: false,
-    accumulator_init: %{},
-    accumulator_pipeline: [],
-    post_reduce_pipeline: []
-  ]
-
-  @type t :: %__MODULE__{}
-
-  # maxContains MUST precede contains so it is put onto the pipeline AFTER the contains
-  # reduction element.
-  # items MUST be last to detect the presence of prefixItems and also clear all filters
-  # in the items = false optimization.
-  @context_filters ~w(minItems maxItems additionalItems prefixItems maxContains minContains contains uniqueItems items)
-  @context_modules Map.new(@context_filters, &{&1, Filter.from_string(&1)})
-
-  @impl true
-  @spec parse(Context.t(), Type.json()) :: t
-  # draft <= 7 refs inhibit type-based analysis
-  def parse(context = %{draft: draft}, %{"$ref" => _}) when draft in ~w(4 6 7) do
-    %__MODULE__{context: context}
-  end
-
-  def parse(context, schema) do
-    %__MODULE__{context: context}
-    |> Tools.collect(@context_filters, fn
-      filter, filter when is_map_key(schema, filter) ->
-        Filter.parse(filter, @context_modules[filter], schema)
-
-      filter, _ ->
-        filter
-    end)
-  end
-
-  @impl true
-  @spec compile(t) :: Macro.t()
-  def compile(filter) do
-    {accumulator_pipeline, index_accumulator} =
-      if :index in Map.keys(filter.accumulator_init) do
-        {
-          filter.accumulator_pipeline ++ [":index"],
-          quote do
-            defp unquote(":index")(acc, _) do
-              %{acc | index: acc.index + 1}
-            end
-          end
-        }
-      else
-        {filter.accumulator_pipeline, :ok}
-      end
-
-    accumulator =
-      if filter.needs_array_in_accumulator do
-        quote do
-          Map.put(unquote(Macro.escape(filter.accumulator_init)), :array, array)
-        end
-      else
-        Macro.escape(filter.accumulator_init)
-      end
-
-    combining =
-      Context.combining(
-        filter.context,
-        quote do
-          array
-        end,
-        quote do
-          path
-        end
-      )
+  def filter(schema, name, pointer) do
+    schema = JsonPointer.resolve!(schema, pointer)
+    call = Tools.pointer_to_fun_name(pointer, authority: name)
 
     quote do
-      defp unquote([])(array, path) when is_list(array) do
-        array
-        |> Enum.reduce(unquote(accumulator), fn item, acc ->
-          Exonerate.pipeline(acc, {path, item}, unquote(accumulator_pipeline))
-        end)
-        |> Exonerate.pipeline({path, array}, unquote(filter.post_reduce_pipeline))
-
-        unquote_splicing(combining)
+      def unquote(call)(content, path) when is_list(content) do
+        :ok
       end
-
-      unquote(index_accumulator)
     end
   end
+
+  def accessories(_, _, _, _), do: []
 end
