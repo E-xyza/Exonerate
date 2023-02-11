04ba5bcce38916d4f038c98537e662e9e6a1db8c
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2023-02-11 12:51:45 -0600

    initial implementation of array reduce-while filter

--- lib/exonerate/filter/contains.ex
+++ lib/exonerate/filter/contains.ex
@@ -1,77 +1,15 @@
 defmodule Exonerate.Filter.Contains do
   @moduledoc false
 
-  @behaviour Exonerate.Filter
-  @derive Exonerate.Compiler
-  @derive {Inspect, except: [:context]}
+  alias Exonerate.Tools
 
-  alias Exonerate.Context
-
-  defstruct [:context, :contains, :min_contains]
-
-  def parse(filter, %{"contains" => _, "minContains" => 0}), do: filter
-
-  def parse(filter = %{context: context}, %{"contains" => _}) do
-    schema =
-      Context.parse(
-        context.schema,
-        JsonPointer.traverse(context.pointer, "contains"),
-        authority: context.authority,
-        format: context.format,
-        draft: context.draft
-      )
-
-    filter = %__MODULE__{
-      context: filter.context,
-      contains: schema,
-      min_contains: is_map_key(schema, "minContains")
-    }
-
-    %{
-      filter
-      | needs_accumulator: true,
-        accumulator_pipeline: [
-          ["contains", ":reduce"] | filter.accumulator_pipeline
-        ],
-        post_reduce_pipeline: ["contains" | filter.post_reduce_pipeline],
-        accumulator_init: Map.put_new(filter.accumulator_init, :contains, 0),
-        filters: [filter | filter.filters]
-    }
-  end
-
-  def compile(filter = %__MODULE__{contains: contains}) do
-    rest =
-      if filter.min_contains do
-        quote do
-        end
-      else
-        quote do
-          defp unquote("contains")(%{contains: 0}, {path, array}) do
-            Exonerate.mismatch(array, path)
-          end
-
-          defp unquote("contains")(acc, {_path, _array}), do: acc
-        end
-      end
-
-    {[],
-     [
-       quote do
-         defp unquote(["contains", ":reduce"])(acc, {path, item}) do
-           try do
-             unquote("contains")(item, path)
-             # yes, it has been seen
-             %{acc | contains: acc.contains + 1}
-           catch
-             # don't update the "contains" value
-             {:error, list} when is_list(list) ->
-               acc
-           end
-         end
-
-         unquote(rest)
-         unquote(Context.compile(contains))
-       end
-     ]}
+  defmacro filter_from_cached(name, pointer, opts) do
+    Tools.maybe_dump(
+      quote do
+        require Exonerate.Context
+        Exonerate.Context.from_cached(unquote(name), unquote(pointer), unquote(opts))
+      end,
+      opts
+    )
   end
 end
--- lib/exonerate/type/array.ex
+++ lib/exonerate/type/array.ex
@@ -2,7 +2,8 @@ defmodule Exonerate.Type.Array do
   alias Exonerate.Tools
 
   @modules %{
-    "items" => Exonerate.Filter.Items
+    "items" => Exonerate.Filter.Items,
+    "contains" => Exonerate.Filter.Contains
   }
 
   @filters Map.keys(@modules)
@@ -21,20 +22,18 @@ defmodule Exonerate.Type.Array do
   end
 
   def filter(schema, name, pointer) do
-    filters =
-      schema
-      |> JsonPointer.resolve!(pointer)
-      |> filter_calls(name, pointer)
-
+    subschema = JsonPointer.resolve!(schema, pointer)
+    class = class_for(subschema)
+    filters = filter_calls(subschema, class, name, pointer)
     call = Tools.pointer_to_fun_name(pointer, authority: name)
 
     quote do
       defp unquote(call)(content, path) when is_list(content) do
         content
-        |> Enum.reduce_while({:ok, 0}, fn
-          item, {:ok, index} ->
+        |> Enum.reduce_while(unquote(initializer_for(class, pointer)), fn
+          item, unquote(accumulator_for(class)) ->
             with unquote_splicing(filters) do
-              {:cont, {:ok, index + 1}}
+              unquote(continuation_for(class))
             else
               error -> {:halt, {error, []}}
             end
@@ -44,21 +43,90 @@ defmodule Exonerate.Type.Array do
     end
   end
 
-  defp filter_calls(schema, name, pointer) do
+  defp class_for(schema) do
+    schema
+    |> Map.take(@filters)
+    |> Map.keys()
+    |> case do
+      ["contains"] -> :contains
+      ["items"] -> :items
+      [] -> nil
+    end
+  end
+
+  defp initializer_for(class, pointer) do
+    case class do
+      # note that "contains" is inverted, we'll generate the error first
+      # and then halt on :ok
+      :contains ->
+        schema_pointer = pointer
+        |> JsonPointer.traverse("contains")
+        |> JsonPointer.to_uri()
+
+        quote do
+          require Exonerate.Tools
+          {Exonerate.Tools.mismatch(content, unquote(schema_pointer), path), []}
+        end
+
+      :items ->
+        {:ok, 0}
+
+      nil ->
+        :ok
+    end
+  end
+
+  defp accumulator_for(class) do
+    case class do
+      :contains ->
+        quote do
+          error
+        end
+
+      :items ->
+        quote do
+          {:ok, index}
+        end
+
+      _ ->
+        quote do
+          _
+        end
+    end
+  end
+
+  defp continuation_for(class) do
+    case class do
+      :contains ->
+        quote do
+          {:cont, error}
+        end
+
+      :items ->
+        quote do
+          {:cont, {:ok, index + 1}}
+        end
+
+      nil ->
+        {:cont, {:ok, []}}
+    end
+  end
+
+  defp filter_calls(schema, class, name, pointer) do
     case Map.take(schema, @filters) do
       empty when empty === %{} ->
         []
 
       filters ->
-        build_filters(filters, name, pointer)
+        build_filters(filters, class, name, pointer)
     end
   end
 
-  defp build_filters(filters, name, pointer) do
-    Enum.map(filters, &filter_for(&1, name, pointer))
+  defp build_filters(filters, class, name, pointer) do
+    Enum.map(filters, &filter_for(&1, class, name, pointer))
   end
 
-  defp filter_for({"items", _}, name, pointer) do
+  defp filter_for({"items", _}, _class, name, pointer) do
     call =
       pointer
       |> JsonPointer.traverse("items")
@@ -66,6 +134,17 @@ defmodule Exonerate.Type.Array do
 
     quote do
       :ok <- unquote(call)(item, Path.join(path, "#{index}"))
+    end
+  end
+
+  defp filter_for({"contains", _}, :contains, name, pointer) do
+    call =
+      pointer
+      |> JsonPointer.traverse("contains")
+      |> Tools.pointer_to_fun_name(authority: name)
+
+    quote do
+      {:error, _} <- unquote(call)(item, Path.join(path, ":any"))
     end
   end
 
