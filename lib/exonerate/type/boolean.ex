7270c149764bd7615bfc377cca8d451c2ff56d6c
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2023-03-06 12:42:21 -0600

    all basic types with no combining are supported

--- lib/exonerate/type/boolean.ex
+++ lib/exonerate/type/boolean.ex
@@ -4,49 +4,36 @@ defmodule Exonerate.Type.Boolean do
   @behaviour Exonerate.Type
 
   alias Exonerate.Combining
-  alias Exonerate.Draft
   alias Exonerate.Tools
 
-  @module_keys Combining.filters()
+  @filters Combining.filters()
 
-  defmacro filter(_, _, _), do: []
-  defmacro accessories(_, _, _), do: []
-
-  defp filters(opts) do
-    if Draft.before?(Keyword.get(opts, :draft, "2020-12"), "2019-09") do
-      @module_keys -- ["$ref"]
-    else
-      @module_keys
-    end
+  defmacro filter(authority, pointer, opts) do
+    __CALLER__
+    |> Tools.subschema(authority, pointer)
+    |> build_filter(authority, pointer, opts)
+    |> Tools.maybe_dump(opts)
   end
 
-  def filter(schema, name, pointer, opts) do
-    filters =
-      schema
-      |> Map.take(filters(opts))
-      |> Enum.map(&filter_for(&1, name, pointer))
+  defp build_filter(context, authority, pointer, opts) do
+    filter_clauses =
+      for filter <- @filters, is_map_key(context, filter) do
+        filter_call = Tools.call(authority, JsonPointer.join(pointer, filter), opts)
 
-    call = Tools.pointer_to_fun_name(pointer, authority: name)
+        quote do
+          :ok <- unquote(filter_call)(boolean, path)
+        end
+      end
 
     quote do
-      defp unquote(call)(content, path) when is_boolean(content) do
-        with unquote_splicing(filters) do
+      defp unquote(Tools.call(authority, pointer, opts))(boolean, path)
+           when is_boolean(boolean) do
+        with unquote_splicing(filter_clauses) do
           :ok
         end
       end
     end
   end
 
-  defp filter_for({filter, _}, name, pointer) do
-    call =
-      pointer
-      |> JsonPointer.join(Combining.adjust(filter))
-      |> Tools.pointer_to_fun_name(authority: name)
-
-    quote do
-      :ok <- unquote(call)(content, path)
-    end
-  end
-
-  def accessories(_, _, _, _), do: []
+  defmacro accessories(_, _, _), do: []
 end
