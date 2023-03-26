d84c06b8011cf027e76f9ed4e458156b2a475452
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2023-03-26 11:42:58 -0500

    expose more draft4-7 tests

--- test/automate_draft4_test.exs
+++ test/automate_draft4_test.exs
@@ -4,7 +4,7 @@ directory_draft4 =
   |> Path.expand()
 
 omit_modules =
-  ~w(refRemote.json definitions.json ref.json infinite-loop-detection.json items.json)
+  ~w(refRemote.json definitions.json ref.json)
 
 omit_describes = [
   # no support for relative uri (for now)
@@ -18,13 +18,14 @@ omit_describes = [
   {"multipleOf.json", 1},
   {"multipleOf.json", 2},
   {"multipleOf.json", 3},
-  # floats don't match ints
-  {"type.json", 0},
-  {"enum.json", 7},
-  {"enum.json", 8}
 ]
 
-omit_tests = []
+omit_tests = [
+  # integer filters do not match float values:
+  {"type.json", {0, 1}},
+  {"enum.json", {7, 2}},
+  {"enum.json", {8, 2}}
+]
 
 ExonerateTest.Automate.directory(
   directory_draft4,
--- test/automate_draft6_test.exs
+++ test/automate_draft6_test.exs
@@ -4,28 +4,23 @@ directory_draft6 =
   |> Path.expand()
 
 omit_modules =
-  ~w(refRemote.json definitions.json ref.json infinite-loop-detection.json items.json)
+  ~w(refRemote.json definitions.json ref.json)
 
 omit_describes = [
-  # no support for relative uri (for now)
+  # references the openAPI schema document, which contains currently unparseable filters.
   {"ref.json", 6},
-  {"ref.json", 11},
-  {"ref.json", 14},
-  {"ref.json", 15},
-  # no support for definitions
-  {"ref.json", 12},
-  {"ref.json", 13},
   # no floating point multiples
   {"multipleOf.json", 1},
   {"multipleOf.json", 2},
-  {"multipleOf.json", 3},
-  # floats don't match ints
-  {"type.json", 0},
-  {"enum.json", 7},
-  {"enum.json", 8}
+  {"multipleOf.json", 3}
 ]
 
-omit_tests = []
+omit_tests = [
+  # integer filters do not match float values:
+  {"type.json", {0, 1}},
+  {"enum.json", {7, 2}},
+  {"enum.json", {8, 2}}
+]
 
 ExonerateTest.Automate.directory(
   directory_draft6,
