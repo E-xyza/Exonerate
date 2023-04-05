101183981e57004546c5add8b9db719d6f5b0a6e
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2023-04-05 10:57:06 -0500

    removes "missing" filter

--- bench/mix.tasks/gpt4_helper.ex
+++ bench/mix.tasks/gpt4_helper.ex
@@ -1,26 +1,6 @@
 defmodule Mix.Tasks.Gpt4Helper do
   @omit ~w(defs.json anchor.json dynamicRef.json id.json refRemote.json)
 
-  @gpt4missing [
-    {"type", "type: array, object or null"},
-    {"ref", "remote ref, containing refs itself"},
-    {"ref", "refs with quote"},
-    {"unevaluatedProperties", "unevaluatedProperties with if-then-else"},
-    {"allOf", "allOf"},
-    {"dependentSchemas", "single dependency"},
-    {"dependentSchemas", "dependencies with escaped characters"},
-    {"enum", "nul characters in strings"},
-    {"format", "validation of URIs"},
-    {"const", "const with {\"a\": false} does not match {\"a\": 0}"},
-    {"const", "const with {\"a\": true} does not match {\"a\": 1}"},
-    {"const", "nul characters in strings"},
-    {"unevaluatedItems", "unevaluatedItems false"},
-    {"unevaluatedItems", "unevaluatedItems with if-then-else"},
-    {"dependentRequired", "single dependency"},
-    {"dependentRequired", "dependencies with escaped characters"},
-    {"oneOf", "oneOf"}
-  ]
-
   alias Benchmark.GPT
   alias Benchmark.Schema
 
@@ -35,7 +15,6 @@ defmodule Mix.Tasks.Gpt4Helper do
     result =
       directory
       |> Schema.stream_from_directory(omit: @omit)
-      |> Stream.filter(&({&1.group, &1.description} in @gpt4missing))
       |> Enum.each(fn schema ->
 
         new_file = result_dir
