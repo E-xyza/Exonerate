db445b03502c273d77d33c7af00c39dcc25fba78
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2023-04-13 12:38:15 -0500

    corrected version of elixir

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Exonerate.MixProject do
   def project do
     [
       app: :exonerate,
-      version: "0.3.0",
-      elixir: "~> 1.11",
+      version: "0.3.1",
+      elixir: "~> 1.14.1",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
