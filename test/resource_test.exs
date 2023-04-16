6dbe799d6fdb80f35f99182c7945d1ac7b4ed2f2
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2023-04-16 11:55:47 -0500

    enables resource test

---
+++ test/resource_test.exs
@@ -0,0 +1,16 @@
+defmodule ExonerateTest.ResourceTest do
+  use ExUnit.Case, async: true
+  require Exonerate
+
+  Exonerate.register_resource(
+    ~s({"foo": {"type": "string"}}),
+    "foo"
+  )
+
+  Exonerate.function_from_resource(:defp, :resource, "foo",  entrypoint: "/foo")
+
+  test "resource" do
+    assert :ok = resource("bar")
+    assert {:error, _} = resource(42)
+  end
+end
